11204f8256e7121888eb1855cb2ddfd8df1881cb
Alan Hernandez <f9.alan@gmail.com>
2026-08-18 16:43:33 -0600

    Update

--- app.py
+++ app.py
@@ -89,8 +89,15 @@ def define_consensus_classes(data: pd.DataFrame) -> pd.DataFrame:
 # ---------------------------------------------------------
 # 3. STREAMLIT USER INTERFACE
 # ---------------------------------------------------------
-st.set_page_config(page_title="MS2DeepScore & MolNetEnhancer", layout="wide")
+st.set_page_config(page_title="MS2DeepScore & MolNetEnhancer", layout="wide",
+    page_icon="⚗️",
+    menu_items={
+        'Report a bug': "mailto:f9.alan@gmail.com",
+        'About': "# Web-App to perform cross-ionization molecular network using MS2DeepScore and integrating the MolNetEnhancer approach using the CANOPUS-derived file."
+    }
+    )
 st.title("MS2DeepScore Molecular Networking & Annotation Pipeline")
+st.image("logo_L125.png", width=300)
 
 tab1, tab2 = st.tabs(["Step 1: Network & Spectral Mapping", "Step 2: Annotations & MolNetEnhancer"])
 
